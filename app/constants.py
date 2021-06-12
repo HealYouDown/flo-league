4f330284ad90fd43d3cd73d3dbb5dda094dd09fa
Jeremy <jeremyregitz@gmail.com>
2021-06-12 11:49:08 +0200

    fix date for season 5

--- app/constants.py
+++ app/constants.py
@@ -4,4 +4,4 @@ K = 50
 
 SEASON = 5
 FIRST_DATE = datetime.datetime(year=2021, month=8, day=1)
-LAST_DATE = datetime.datetime(year=2021, month=9, day=9)
+LAST_DATE = datetime.datetime(year=2021, month=9, day=19)
